9c31adc47fad17685bf8daf3faf40d4e5557ddfa
XoDeR <xoder.vs@gmail.com>
2025-10-09 21:54:24 +0200

    added nav menu

--- frontend/src/app/nav-menu/nav-menu.ts
+++ frontend/src/app/nav-menu/nav-menu.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // includes ngClass, ngIf, etc.
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.scss'
 })
 export class NavMenu {
+  isExpanded = false;
 
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
