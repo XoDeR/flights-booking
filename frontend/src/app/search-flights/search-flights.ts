00c5495eb4a5993c73176bb0d3c03b791714b810
XoDeR <xoder.vs@gmail.com>
2025-10-09 22:31:18 +0200

    displaying a list of data in a html template

--- frontend/src/app/search-flights/search-flights.ts
+++ frontend/src/app/search-flights/search-flights.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // includes ngClass, ngIf, etc.
 
 @Component({
   selector: 'app-search-flights',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class SearchFlights {
 
+  // test data
+  searchResults: any = [
+    "American Airlines",
+    "British Airways",
+    "Lufthansa",
+  ];
 }
