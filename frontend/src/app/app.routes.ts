c636be38b8dbf8225c38ad8c9cb1176ea1218dd2
XoDeR <xoder.vs@gmail.com>
2025-10-09 22:03:15 +0200

    added search-flights to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SearchFlights } from './search-flights/search-flights';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchFlights },
+];
+
+
