d7749faae020a7c58da601d15757f66b5440a009
XoDeR <xoder.vs@gmail.com>
2025-10-11 19:10:31 +0200

    displaying all flight data and styling added

--- frontend/src/app/search-flights/search-flights.ts
+++ frontend/src/app/search-flights/search-flights.ts
@@ -10,9 +10,49 @@ import { CommonModule } from '@angular/common'; // includes ngClass, ngIf, etc.
 export class SearchFlights {
 
   // test data
-  searchResults: any = [
+  searchResultsV01: any = [
     "American Airlines",
     "British Airways",
     "Lufthansa",
   ];
+
+  searchResults: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: "Los-Angeles" },
+      arrival: { time: Date.now().toString(), place: "Istanbul" },
+      price: "350",
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Munchen" },
+      arrival: { time: Date.now().toString(), place: "Schiphol" },
+      price: "600",
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 50,
+      departure: { time: Date.now().toString(), place: "London" },
+      arrival: { time: Date.now().toString(), place: "Valencia" },
+      price: "700",
+    }
+  ];
+
+
+}
+
+// rm -- read module, just a convention for interfaces' names
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
