18703c5d15bf62e1c15fbc26f3427b905afac439
XoDeR <xoder.vs@gmail.com>
2025-10-20 22:03:30 +0200

    generated register-passenger component and added it to routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { SearchFlights } from './search-flights/search-flights';
 import { BookFlight } from './book-flight/book-flight';
+import { RegisterPassenger } from './register-passenger/register-passenger';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'search-flights', pathMatch: 'full' },
   { path: 'search-flights', component: SearchFlights },
   { path: 'book-flight/:flightId', component: BookFlight },
+  { path: 'register-passenger', component: RegisterPassenger },
 ];
 
 
---
+++ frontend/src/app/register-passenger/register-passenger.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-passenger',
+  imports: [],
+  templateUrl: './register-passenger.html',
+  styleUrl: './register-passenger.scss'
+})
+export class RegisterPassenger {
+
+}
