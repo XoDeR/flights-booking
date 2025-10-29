3f26975c571e8ab3c6dac389069e7d35d7e517f7
XoDeR <xoder.vs@gmail.com>
2025-10-29 19:50:41 +0100

    attached angular reactive form and bounded it to a html form

--- frontend/src/app/register-passenger/register-passenger.ts
+++ frontend/src/app/register-passenger/register-passenger.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { PassengerService } from '../api/services/passenger.service';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register-passenger',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-passenger.html',
   styleUrl: './register-passenger.scss'
 })
 export class RegisterPassenger {
-  constructor(private passengerService: PassengerService) { }
+  form!: FormGroup;
+
+  constructor(private passengerService: PassengerService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: [''],
+      firstName: [''],
+      lastName: [''],
+      isFemale: [true],
+    });
+  }
 
   register() {
+    console.log("Form fields: ", this.form.value);
     this.passengerService.registerPassenger();
   }
 }
