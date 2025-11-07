bcbd4ab3861006b2684af5f66d7151b51ebd38c2
XoDeR <xoder.vs@gmail.com>
2025-11-07 19:39:53 +0100

    new my-bookings component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SearchFlights } from './search-flights/search-flights';
 import { BookFlight } from './book-flight/book-flight';
 import { RegisterPassenger } from './register-passenger/register-passenger';
+import { MyBookings } from './my-bookings/my-bookings';
 
 
 export const routes: Routes = [
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: 'search-flights', component: SearchFlights },
   { path: 'book-flight/:flightId', component: BookFlight },
   { path: 'register-passenger', component: RegisterPassenger },
+  { path: 'my-bookings', component: MyBookings },
 ];
 
 
