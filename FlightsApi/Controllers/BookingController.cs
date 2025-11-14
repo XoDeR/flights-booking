fd7937e8e0fbb235c6aae92472ee36945e3aff59
XoDeR <xoder.vs@gmail.com>
2025-11-14 17:47:04 +0100

    booking controller list (get all) endpoint

--- FlightsApi/Controllers/BookingController.cs
+++ FlightsApi/Controllers/BookingController.cs
@@ -14,5 +14,29 @@ namespace FlightsApi.Controllers
         {
             _entities = entities;
         }
+
+        [HttpGet("{email}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(IEnumerable<BookingRm>), 200)]
+        public ActionResult<IEnumerable<BookingRm>> List(string email)
+        {
+            var bookings = _entities.Flights
+                .ToList()
+                .SelectMany(f => f.Bookings
+                    .Where(b => b.PassengerEmail == email)
+                    .Select(b => new BookingRm(
+                        f.Id,
+                        f.Airline,
+                        f.Price.ToString(),
+                        new TimePlaceRm(f.Arrival.Place, f.Arrival.Time),
+                        new TimePlaceRm(f.Departure.Place, f.Departure.Time),
+                        b.NumberOfSeats,
+                        email
+                    ))
+            );
+
+            return Ok(bookings);
+        }
     }
 }
